341b27789654b1b539f284527d8b87b67b8e2a1e
ChokunPlayZ <49745982+ChokunPlayZ@users.noreply.github.com>
2020-05-15 18:18:17 +0700

    Main Update Script
    V.1 Script

---
+++ update.sh
@@ -0,0 +1,104 @@
+clear
+# Landing Script
+echo "Rspi Updater v.1.1"
+echo "By ChokunPlayZ"
+
+sleep 0.5
+
+echo "If you really want to update your Rapberry pi"
+echo "Press [Enter] on your keybord"
+echo "If not Press Ctrl + C on your Keybord"
+read reply
+
+# Starting Coutdown Script
+
+sleep 1
+clear
+
+echo "Starting in  5"
+sleep 1
+clear
+
+echo "Starting in  4"
+sleep 1
+clear
+
+echo "Starting in  3"
+sleep 1
+clear
+
+echo "Starting in  2"
+sleep 1
+clear
+
+echo "Starting in  1"
+sleep 1
+clear
+
+echo "Starting..."
+sleep 0.5
+
+# Udate Script
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Restart Coutdown Script
+clear
+
+echo "Do You Want To Restart Your Raspberry Pi ?"
+echo "If Yes Press [Enter] on your keybord"
+echo "If no Press [Ctrl] + [C] on your keybord to exit"
+echo "There will be 5 Seconds Countdown before restart"
+echo "So Dont Worry you can exit Later"
+read reply
+sleep 1
+clear
+
+echo "Restarting Your Raspberry Pi In"
+echo "5 Seconds"
+echo "You Can Press Ctrl + C To Quit"
+echo "Or Continue To Restart"
+sleep 1
+clear
+
+echo "Restarting Your Raspberry Pi In"
+echo "4 Seconds"
+echo "You Can Press Ctrl + C To Quit"
+echo "Or Continue To Restart"
+sleep 1
+clear
+
+echo "Restarting Your Raspberry Pi In"
+echo "3 Seconds"
+echo "You Can Press Ctrl + C To Quit"
+echo "Or Continue To Restart"
+sleep 1
+clear
+
+echo "Restarting Your Raspberry Pi In"
+echo "2 Seconds"
+echo "You Can Press Ctrl + C To Quit"
+echo "Or Continue To Restart"
+sleep 1
+clear
+
+echo "Restarting Your Raspberry Pi In"
+echo "1 Seconds"
+echo "You Can Press Ctrl + C To Quit"
+echo "Or Continue To Restart"
+sleep 1
+clear
+
+#Restart Command
+
+echo "Restart Commancing"
+sleep 0.5
+echo "Please Wait 10-20 Seconds"
+echo "After Your Session Disconnected"
+echo "Then You can log back in"
+sleep 1
+
+sudo restart
+
+
